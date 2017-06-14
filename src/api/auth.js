c538bea53a4d759482d97ce3e9268066e6dc8274
auipw <aui_pw@hotmail.com>
2017-06-14 16:12:40 +0700

    changepasaword2

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,6 +3,8 @@ import Boom from 'boom';
 import { User } from '../models';
 
 const passwordPattern = /^(?=.*\d)(?=.*[A-Z]).{8,20}/;
+let email_user;
+let userID;
 
 const login = {
   tags: ['auth', 'api'],
@@ -14,9 +16,11 @@ const login = {
   },
   handler: (request, reply) => {
     const { email, password } = request.payload;
+    console.log('test1');
 
     User.findOne({ email })
       .then((user) => {
+        email_user = email;
         if (!user) {
           reply(Boom.unauthorized('Invalid email or password'));
         } else {
@@ -26,7 +30,7 @@ const login = {
             const { auth } = request.server.app.services;
             const token = auth.createAuthToken(user);
 
-            reply({ token });
+            reply({token});
           }
         }
       });
@@ -41,9 +45,52 @@ const logout = {
   },
 };
 
+const changepassword = {
+  tags: ['api'],
+  validate: {
+    payload: {
+      email: Joi.string().required().email(),
+      password: Joi.string().required().trim().regex(passwordPattern),
+      newpassword: Joi.string().required().trim().regex(passwordPattern),
+      repassword: Joi.string().required().trim().regex(passwordPattern),
+    },
+  },
+  handler: (request, reply) => {
+    const { email, password, newpassword, repassword } = request.payload;
+
+    User.findOne({ email })
+      .then((user) => {
+
+        if (user) {
+          if(user.comparePassword(password)){
+            if(newpassword === repassword){
+              user.password = newpassword;
+              user.save(function(err){
+                if (err) throw err;
+                reply('Password has changed.');
+              });
+            }else{
+              reply(Boom.badData('Please fill new password again!'));
+            }
+          }else{
+            reply(Boom.badData('Password is not correct!'));
+          }
+        }else{
+          reply(Boom.badData('Your e-mail is not correct!'));
+        }
+      });
+  },
+};
+
 export default function(server) {
-  server.route([
-    { method: 'POST', path: '/login', config: login },
-    { method: 'GET', path: '/logout', config: logout },
-  ]);
+  User.findOne({email: email_user},(err,obj)=>{
+    if(!email_user == null){
+      const userID = obj._id;
+    }
+    server.route([
+      { method: 'POST', path: '/login', config: login },
+      { method: 'GET', path: '/logout', config: logout }, 
+      { method: 'POST', path: '/user/'+ userID +'/changepassword', config: changepassword },
+    ]);
+  });
 }
--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,12 @@ import Register from './register';
 import Auth from './auth';
 import FileHandler from './file';
 import Remove from './remove';
-import Changepassword from './changepassword';
 
 export const register = (server, options, next) => {
   Register(server);
   Auth(server);
   FileHandler(server);
   Remove(server);
-  Changepassword(server);
   next();
 };
 
