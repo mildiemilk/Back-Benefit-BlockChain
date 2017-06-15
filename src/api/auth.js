bc41d6662ac5e3445b6577db3f90013fcd03b19d
auipw <aui_pw@hotmail.com>
2017-06-15 17:14:05 +0700

    changepassword3

--- src/api/auth.js
+++ src/api/auth.js
@@ -16,19 +16,13 @@ const login = {
   },
   handler: (request, reply) => {
     const { email, password } = request.payload;
-<<<<<<< HEAD
-    console.log('test1');
 
     User.findOne({ email })
       .then((user) => {
-        email_user = email;
-=======
-    User.findOne({ email })
-      .then((user) => {
+        
         if(user.removedAt != null){
           console.log("This user is removed");
         }
->>>>>>> 8912bc44ca96de601924d751ea4dbd331d6bb5d4
         if (!user) {
           reply(Boom.unauthorized('Invalid email or password'));
         } else {
@@ -37,7 +31,6 @@ const login = {
           } else {
             const { auth } = request.server.app.services;
             const token = auth.createAuthToken(user);
-
             reply({token});
           }
         }
@@ -47,58 +40,51 @@ const login = {
 
 const logout = {
   tags: ['auth', 'api'],
-  auth: 'jwt',
+  auth: 'jwt', 
   handler: (request, reply) => {
     reply("OK");
   },
 };
 
 const changepassword = {
-  tags: ['api'],
+  tags: ['auth','api'],
+  auth: 'jwt',
+
   validate: {
     payload: {
-      email: Joi.string().required().email(),
       password: Joi.string().required().trim().regex(passwordPattern),
       newpassword: Joi.string().required().trim().regex(passwordPattern),
       repassword: Joi.string().required().trim().regex(passwordPattern),
     },
   },
+
   handler: (request, reply) => {
-    const { email, password, newpassword, repassword } = request.payload;
-
-    User.findOne({ email })
-      .then((user) => {
-
-        if (user) {
-          if(user.comparePassword(password)){
-            if(newpassword === repassword){
-              user.password = newpassword;
-              user.save(function(err){
-                if (err) throw err;
-                reply('Password has changed.');
-              });
-            }else{
-              reply(Boom.badData('Please fill new password again!'));
-            }
-          }else{
-            reply(Boom.badData('Password is not correct!'));
-          }
+    const { password, newpassword, repassword } = request.payload;
+    const { user } = request.auth.credentials;
+    if (user) {
+      if(user.comparePassword(password)){
+        if(newpassword === repassword){
+          user.password = newpassword;
+          user.save(function(err){
+            if (err) throw err;
+            reply('Password has changed.');
+          });
         }else{
-          reply(Boom.badData('Your e-mail is not correct!'));
+          reply(Boom.badData('Please fill new password again!'));
         }
-      });
+      }else{
+        reply(Boom.badData('Password is not correct!'));
+      }
+    }else{
+      reply(Boom.badData('Your e-mail is not correct!'));
+    }
   },
 };
 
-export default function(server) {
-  User.findOne({email: email_user},(err,obj)=>{
-    if(!email_user == null){
-      const userID = obj._id;
-    }
-    server.route([
-      { method: 'POST', path: '/login', config: login },
-      { method: 'GET', path: '/logout', config: logout }, 
-      { method: 'POST', path: '/user/'+ userID +'/changepassword', config: changepassword },
-    ]);
-  });
+export default function Auth(server) {
+  server.route([
+    { method: 'POST', path: '/login', config: login },
+    { method: 'GET', path: '/logout', config: logout }, 
+    { method: 'POST', path: '/user/changepassword', config: changepassword },
+  ]);
 }
