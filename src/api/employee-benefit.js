ae5118fe2b08c0c8736cf0ccf4f838b4685edbf4
doschnn <detective-i@hotmail.com>
2017-09-12 14:51:59 +0700

    api employee get-claim-status/history

--- src/api/employee-benefit.js
+++ src/api/employee-benefit.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 import Boom from 'boom';
-import { EmployeeGroup, BenefitPlan } from '../models';
+import { EmployeeGroup, BenefitPlan, LogUserClaim } from '../models';
 
 const getAllBenefit = {
   tags: ['api'],
@@ -20,8 +20,37 @@ const getAllBenefit = {
   },
 };
 
+const getClaimStatus = {
+  tags: ['api'],
+  auth: 'jwt',
+  handler: (request, reply) => {
+    const { user } = request.auth.credentials;
+    const today = new Date();
+    const afterSevenDay = new Date();
+    afterSevenDay.setDate(today.getDate() - 7);
+    LogUserClaim.find({ user: user._id, $and:[{createdAt:{$lte:today}},{createdAt:{$gte:afterSevenDay}}] }, '-createdAt -updatedAt -deleted').then((logClaim) => {
+      reply(logClaim);
+    });
+  },
+};
+
+const getClaimHistory = {
+  tags: ['api'],
+  auth: 'jwt',
+  handler: (request, reply) => {
+    const { user } = request.auth.credentials;
+    const afterSevenDay = new Date();
+    afterSevenDay.setDate(afterSevenDay.getDate() - 7);
+    LogUserClaim.find({ user: user._id, createdAt:{$lt:afterSevenDay }}, '-createdAt -updatedAt -deleted').then((logClaim) => {
+      reply(logClaim);
+    });
+  },
+};
+
 export default function(app) {
   app.route([
     { method: 'GET', path: '/employee/get-all-benefit', config: getAllBenefit },
+    { method: 'GET', path: '/employee/get-claim-status', config: getClaimStatus},
+    { method: 'GET', path: '/employee/get-claim-history', config: getClaimHistory},
   ]);
 }
