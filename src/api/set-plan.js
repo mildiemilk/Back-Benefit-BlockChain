156aa3cc33e99ab0258bd53319c14c5f4f222672
doschnn <detective-i@hotmail.com>
2017-09-15 11:37:54 +0700

    updated api edit insurer plan

--- src/api/set-plan.js
+++ src/api/set-plan.js
@@ -29,7 +29,7 @@ const createPlan = {
   },
 };
 
-const editPlan = {
+const editMasterPlan = {
   tags: ['api'],
   auth: 'jwt',
 
@@ -80,7 +80,6 @@ const editPlan = {
     params: {
       planId: Joi.number().integer().required(),
       typeEdit: Joi.string().required(),
-      editBy: Joi.string().valid('company', 'insurer').required(),
     },
   },
   handler: (request, reply) => {
@@ -92,25 +91,19 @@ const editPlan = {
       ipdCoPayDeductable, ipdCoPayMixPercentage, ipdCoPayMixNotExceed, ipdCoPayMixYear, opdPerYear, opdPerTime, opdTimeNotExceedPerYear,
       opdCoPay, opdCoPayQuota, opdCoPayDeductable, opdCoPayMixPercentage, opdCoPayMixNotExceed, opdCoPayMixYear, dentalPerYear,
       lifePerYear, lifeTimeOfSalary, lifeNotExceed } = request.payload;
-    const { planId, typeEdit, editBy } = request.params;
+    const { planId, typeEdit } = request.params;
     const { user } = request.auth.credentials;
-    let planType;
-
-    switch(editBy) {
-      case 'company' : planType = MasterPlan; break;
-      case 'insurer' : planType = InsurerPlan; break;
-    }
 
     Role.findOne({ _id: user.role }).then((thisRole) => {
       const role =  thisRole.roleName;
-      if(role == 'HR' || role === 'Insurer'){
+      if(role == 'HR'){
         switch(typeEdit) {
-          case 'profilePlan' : planType.findOneAndUpdate({ planId: planId }, { $set:
+          case 'profilePlan' : MasterPlan.findOneAndUpdate({ planId: planId }, { $set:
           {
             planName: planName,
             employeeOfPlan: employeeOfPlan,
           }}, () => reply({message: 'edit Profile Plan complete!'})); break;
-          case 'ipd' : planType.findOneAndUpdate({ planId: planId }, { $set:
+          case 'ipd' : MasterPlan.findOneAndUpdate({ planId: planId }, { $set:
           {
             ipdType: ipdType,
             ipdLumsumPerYear: ipdLumsumPerYear,
@@ -139,7 +132,7 @@ const editPlan = {
             ipdCoPayMixNotExceed: ipdCoPayMixNotExceed,
             ipdCoPayMixYear: ipdCoPayMixYear,
           }}, () => reply({message: 'edit IPD complete!'})); break;
-          case 'opd' : planType.findOneAndUpdate({ planId: planId }, { $set:
+          case 'opd' : MasterPlan.findOneAndUpdate({ planId: planId }, { $set:
           {
             opdPerYear: opdPerYear,
             opdPerTime: opdPerTime,
@@ -151,10 +144,10 @@ const editPlan = {
             opdCoPayMixNotExceed: opdCoPayMixNotExceed,
             opdCoPayMixYear: opdCoPayMixYear,
           }}, () => reply({message: 'edit OPD complete!'})); break;
-          case 'dental' :  planType.findOneAndUpdate({ planId: planId }, { $set: { dentalPerYear: dentalPerYear }},() => {
+          case 'dental' :  MasterPlan.findOneAndUpdate({ planId: planId }, { $set: { dentalPerYear: dentalPerYear }},() => {
             reply({message: 'edit Dental complete!'});
           }); break;
-          case 'life' : planType.findOneAndUpdate({ planId: planId }, { $set:
+          case 'life' : MasterPlan.findOneAndUpdate({ planId: planId }, { $set:
           {
             lifePerYear: lifePerYear,
             lifeTimeOfSalary: lifeTimeOfSalary,
@@ -162,7 +155,125 @@ const editPlan = {
           }}, () => reply({message: 'edit Life complete!'})); break;
         }
       }else{
-        reply({ message:'หน้านี้สำหรับ HR หรือ Broker เท่านั้น'});
+        reply({ message:'หน้านี้สำหรับ HR เท่านั้น'});
+      }
+    });
+  },
+};
+
+const editInsurerPlan = {
+  tags: ['api'],
+  auth: 'jwt',
+
+  validate: {
+    payload: {
+      planName: Joi.string(),
+      employeeOfPlan: Joi.number().integer(),
+      ipdType: Joi.string().allow(null),
+      ipdLumsumPerYear: Joi.number().allow(null),
+      ipdLumsumPerTime: Joi.number().allow(null),
+      ipdLumsumTimeNotExceedPerYear: Joi.number().allow(null),
+      rbLumsumRoomPerNight: Joi.number().allow(null),
+      rbLumsumNigthNotExceedPerYear: Joi.number().allow(null),
+      rbLumsumPayNotExceedPerNight: Joi.number().allow(null),
+      rbLumsumPayNotExceedPerYear: Joi.number().allow(null),
+      rbSchedulePatient: Joi.number().allow(null),
+      rbScheduleIntensiveCarePatient: Joi.number().allow(null),
+      rbScheduleDoctor: Joi.number().allow(null),
+      rbScheduleSurgerySchedule: Joi.number().allow(null),
+      rbScheduleSurgeryNonSchedule: Joi.number().allow(null),
+      rbScheduleService: Joi.number().allow(null),
+      rbScheduleSmallSurgery: Joi.number().allow(null),
+      rbScheduleAdviser: Joi.number().allow(null),
+      rbScheduleAmbulance: Joi.number().allow(null),
+      rbScheduleAccident: Joi.number().allow(null),
+      rbScheduleTreatment: Joi.number().allow(null),
+      rbScheduleTransplant: Joi.number().allow(null),
+      ipdCoPay: Joi.boolean(),
+      ipdCoPayQuota: Joi.number().allow(null),
+      ipdCoPayDeductable: Joi.number().allow(null),
+      ipdCoPayMixPercentage: Joi.number().allow(null),
+      ipdCoPayMixNotExceed: Joi.number().allow(null),
+      ipdCoPayMixYear: Joi.number().integer().allow(null),
+      opdPerYear: Joi.number().allow(null),
+      opdPerTime: Joi.number().allow(null),
+      opdTimeNotExceedPerYear: Joi.number().allow(null),
+      opdCoPay: Joi.boolean(),
+      opdCoPayQuota: Joi.number().allow(null),
+      opdCoPayDeductable: Joi.number().allow(null),
+      opdCoPayMixPercentage: Joi.number().allow(null),
+      opdCoPayMixNotExceed: Joi.number().allow(null),
+      opdCoPayMixYear: Joi.number().integer().allow(null),
+      dentalPerYear: Joi.number().allow(null),
+      lifePerYear: Joi.number().allow(null),
+      lifeTimeOfSalary: Joi.number().allow(null),
+      lifeNotExceed: Joi.number().allow(null),
+    },
+    params: {
+      planId: Joi.number().integer().required(),
+      typeEdit: Joi.string().required(),
+    },
+  },
+  handler: (request, reply) => {
+    const { planName, employeeOfPlan, ipdType, ipdLumsumPerYear, ipdLumsumPerTime, ipdLumsumTimeNotExceedPerYear, rbLumsumRoomPerNight,
+      rbLumsumNigthNotExceedPerYear, rbLumsumPayNotExceedPerNight, rbLumsumPayNotExceedPerYear,
+      rbSchedulePatient, rbScheduleIntensiveCarePatient, rbScheduleDoctor, rbScheduleSurgerySchedule, rbScheduleSurgeryNonSchedule,
+      rbScheduleService, rbScheduleSmallSurgery, rbScheduleAdviser, rbScheduleAmbulance,
+      rbScheduleAccident, rbScheduleTreatment, rbScheduleTransplant, ipdCoPay, ipdCoPayQuota,
+      ipdCoPayDeductable, ipdCoPayMixPercentage, ipdCoPayMixNotExceed, ipdCoPayMixYear, opdPerYear, opdPerTime, opdTimeNotExceedPerYear,
+      opdCoPay, opdCoPayQuota, opdCoPayDeductable, opdCoPayMixPercentage, opdCoPayMixNotExceed, opdCoPayMixYear, dentalPerYear,
+      lifePerYear, lifeTimeOfSalary, lifeNotExceed } = request.payload;
+    const { planId } = request.params;
+    const { user } = request.auth.credentials;
+
+    Role.findOne({ _id: user.role }).then((thisRole) => {
+      const role =  thisRole.roleName;
+      if(role === 'Insurer'){
+        InsurerPlan.findOneAndUpdate({ planId: planId }, { $set:
+        { planName: planName,
+          employeeOfPlan: employeeOfPlan,
+          ipdType: ipdType,
+          ipdLumsumPerYear: ipdLumsumPerYear,
+          ipdLumsumPerTime: ipdLumsumPerTime,
+          ipdLumsumTimeNotExceedPerYear: ipdLumsumTimeNotExceedPerYear,
+          rbLumsumRoomPerNight: rbLumsumRoomPerNight,
+          rbLumsumNigthNotExceedPerYear: rbLumsumNigthNotExceedPerYear,
+          rbLumsumPayNotExceedPerNight: rbLumsumPayNotExceedPerNight,
+          rbLumsumPayNotExceedPerYear: rbLumsumPayNotExceedPerYear,
+          rbSchedulePatient: rbSchedulePatient,
+          rbScheduleIntensiveCarePatient: rbScheduleIntensiveCarePatient,
+          rbScheduleDoctor: rbScheduleDoctor,
+          rbScheduleSurgerySchedule: rbScheduleSurgerySchedule,
+          rbScheduleSurgeryNonSchedule: rbScheduleSurgeryNonSchedule,
+          rbScheduleService: rbScheduleService,
+          rbScheduleSmallSurgery: rbScheduleSmallSurgery,
+          rbScheduleAdviser: rbScheduleAdviser,
+          rbScheduleAmbulance: rbScheduleAmbulance,
+          rbScheduleAccident: rbScheduleAccident,
+          rbScheduleTreatment: rbScheduleTreatment,
+          rbScheduleTransplant: rbScheduleTransplant,
+          ipdCoPay: ipdCoPay,
+          ipdCoPayQuota: ipdCoPayQuota,
+          ipdCoPayDeductable: ipdCoPayDeductable,
+          ipdCoPayMixPercentage: ipdCoPayMixPercentage,
+          ipdCoPayMixNotExceed: ipdCoPayMixNotExceed,
+          ipdCoPayMixYear: ipdCoPayMixYear,
+          opdPerYear: opdPerYear,
+          opdPerTime: opdPerTime,
+          opdTimeNotExceedPerYear: opdTimeNotExceedPerYear,
+          opdCoPay: opdCoPay,
+          opdCoPayQuota: opdCoPayQuota,
+          opdCoPayDeductable: opdCoPayDeductable,
+          opdCoPayMixPercentage: opdCoPayMixPercentage,
+          opdCoPayMixNotExceed: opdCoPayMixNotExceed,
+          opdCoPayMixYear: opdCoPayMixYear,
+          dentalPerYear: dentalPerYear,
+          lifePerYear: lifePerYear,
+          lifeTimeOfSalary: lifeTimeOfSalary,
+          lifeNotExceed: lifeNotExceed,
+        }}, () => reply({message: 'edit plan complete!'})); 
+      } else {
+        reply({ message:'หน้านี้สำหรับ Insurer เท่านั้น'});
       }
     });
   },
@@ -351,7 +462,8 @@ export default function(app) {
   app.route([
     { method: 'POST', path: '/company/create-plan', config: createPlan },
     { method: 'POST', path: '/insurer/extended-plan/{planId}', config: extendedPlan },
-    { method: 'PUT', path: '/{editBy}/edit-plan/{planId}/{typeEdit}', config: editPlan },
+    { method: 'PUT', path: '/company/edit-plan/{planId}/{typeEdit}', config: editMasterPlan },
+    { method: 'PUT', path: '/insurer/edit-plan/{planId}', config: editInsurerPlan },
     { method: 'DELETE', path: '/{editBy}/delete-plan/{planId}', config: deletePlan },
     { method: 'POST', path: '/company/copy-plan/{planId}', config: copyPlan },
     { method: 'GET', path: '/company/get-all-plan', config: getAllPlan },
