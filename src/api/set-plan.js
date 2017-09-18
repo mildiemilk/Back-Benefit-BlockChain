ed89c3a43e1319af0098e0bad015da1059d3db5a
doschnn <detective-i@hotmail.com>
2017-09-18 13:03:47 +0700

    fixed insurer extended plan

--- src/api/set-plan.js
+++ src/api/set-plan.js
@@ -399,64 +399,80 @@ const extendedPlan = {
   auth: 'jwt',
 
   validate: {
+    payload: {
+      planName: Joi.string(),
+      employeeOfPlan: Joi.number().integer(),
+      ipdType: Joi.string().allow(null),
+      ipdLumsumPerYear: Joi.number().allow(null),
+      ipdLumsumPerTime: Joi.number().allow(null),
+      ipdLumsumTimeNotExceedPerYear: Joi.number().allow(null),
+      rbLumsumRoomPerNight: Joi.number().allow(null),
+      rbLumsumNigthNotExceedPerYear: Joi.number().allow(null),
+      rbLumsumPayNotExceedPerNight: Joi.number().allow(null),
+      rbLumsumPayNotExceedPerYear: Joi.number().allow(null),
+      rbSchedulePatient: Joi.number().allow(null),
+      rbScheduleIntensiveCarePatient: Joi.number().allow(null),
+      rbScheduleDoctor: Joi.number().allow(null),
+      rbScheduleSurgerySchedule: Joi.number().allow(null),
+      rbScheduleSurgeryNonSchedule: Joi.number().allow(null),
+      rbScheduleService: Joi.number().allow(null),
+      rbScheduleSmallSurgery: Joi.number().allow(null),
+      rbScheduleAdviser: Joi.number().allow(null),
+      rbScheduleAmbulance: Joi.number().allow(null),
+      rbScheduleAccident: Joi.number().allow(null),
+      rbScheduleTreatment: Joi.number().allow(null),
+      rbScheduleTransplant: Joi.number().allow(null),
+      ipdCoPay: Joi.boolean(),
+      ipdCoPayQuota: Joi.number().allow(null),
+      ipdCoPayDeductable: Joi.number().allow(null),
+      ipdCoPayMixPercentage: Joi.number().allow(null),
+      ipdCoPayMixNotExceed: Joi.number().allow(null),
+      ipdCoPayMixYear: Joi.number().integer().allow(null),
+      opdPerYear: Joi.number().allow(null),
+      opdPerTime: Joi.number().allow(null),
+      opdTimeNotExceedPerYear: Joi.number().allow(null),
+      opdCoPay: Joi.boolean(),
+      opdCoPayQuota: Joi.number().allow(null),
+      opdCoPayDeductable: Joi.number().allow(null),
+      opdCoPayMixPercentage: Joi.number().allow(null),
+      opdCoPayMixNotExceed: Joi.number().allow(null),
+      opdCoPayMixYear: Joi.number().integer().allow(null),
+      dentalPerYear: Joi.number().allow(null),
+      lifePerYear: Joi.number().allow(null),
+      lifeTimeOfSalary: Joi.number().allow(null),
+      lifeNotExceed: Joi.number().allow(null),
+    },
     params: {
       planId: Joi.number().integer().required(),
     },
   },
 
   handler: (request, reply) => {
+    const { planName, employeeOfPlan, ipdType, ipdLumsumPerYear, ipdLumsumPerTime, ipdLumsumTimeNotExceedPerYear, rbLumsumRoomPerNight,
+      rbLumsumNigthNotExceedPerYear, rbLumsumPayNotExceedPerNight, rbLumsumPayNotExceedPerYear,
+      rbSchedulePatient, rbScheduleIntensiveCarePatient, rbScheduleDoctor, rbScheduleSurgerySchedule, rbScheduleSurgeryNonSchedule,
+      rbScheduleService, rbScheduleSmallSurgery, rbScheduleAdviser, rbScheduleAmbulance,
+      rbScheduleAccident, rbScheduleTreatment, rbScheduleTransplant, ipdCoPay, ipdCoPayQuota,
+      ipdCoPayDeductable, ipdCoPayMixPercentage, ipdCoPayMixNotExceed, ipdCoPayMixYear, opdPerYear, opdPerTime, opdTimeNotExceedPerYear,
+      opdCoPay, opdCoPayQuota, opdCoPayDeductable, opdCoPayMixPercentage, opdCoPayMixNotExceed, opdCoPayMixYear, dentalPerYear,
+      lifePerYear, lifeTimeOfSalary, lifeNotExceed } = request.payload;
     const { planId } = request.params;
     const { user } = request.auth.credentials;
     MasterPlan.findOne({ planId })
       .then((masterPlan) => {
-        const planName = masterPlan.planName;
-        const company = masterPlan.company;
-        const employeeOfPlan = masterPlan.employeeOfPlan;
         const extendedFrom = masterPlan._id;
         const createdBy = user._id;
-        let newPlan = new  InsurerPlan({ planName, company, employeeOfPlan, extendedFrom, createdBy });
+        const company = user.company.detail;
+        let newPlan = new  InsurerPlan({ company, employeeOfPlan, extendedFrom, createdBy, planName, ipdType, ipdLumsumPerYear, ipdLumsumPerTime, ipdLumsumTimeNotExceedPerYear, rbLumsumRoomPerNight,
+          rbLumsumNigthNotExceedPerYear, rbLumsumPayNotExceedPerNight, rbLumsumPayNotExceedPerYear,
+          rbSchedulePatient, rbScheduleIntensiveCarePatient, rbScheduleDoctor, rbScheduleSurgerySchedule, rbScheduleSurgeryNonSchedule,
+          rbScheduleService, rbScheduleSmallSurgery, rbScheduleAdviser, rbScheduleAmbulance,
+          rbScheduleAccident, rbScheduleTreatment, rbScheduleTransplant, ipdCoPay, ipdCoPayQuota,
+          ipdCoPayDeductable, ipdCoPayMixPercentage, ipdCoPayMixNotExceed, ipdCoPayMixYear, opdPerYear, opdPerTime, opdTimeNotExceedPerYear,
+          opdCoPay, opdCoPayQuota, opdCoPayDeductable, opdCoPayMixPercentage, opdCoPayMixNotExceed, opdCoPayMixYear, dentalPerYear,
+          lifePerYear, lifeTimeOfSalary, lifeNotExceed});
         newPlan.save().then(() => {
-          InsurerPlan.findOneAndUpdate({ planId: newPlan.planId }, { $set:
-          {
-            ipdLumsumPerYear: masterPlan.ipdLumsumPerYear,
-            ipdLumsumPerTime: masterPlan.ipdLumsumPerTime,
-            ipdLumsumTimeNotExceedPerYear: masterPlan.ipdLumsumTimeNotExceedPerYear,
-            rbLumsumRoomPerNight: masterPlan.rbLumsumRoomPerNight,
-            rbLumsumNigthNotExceedPerYear: masterPlan.rbLumsumPayNotExceedPerYear,
-            rbLumsumPayNotExceedPerNight: masterPlan.rbLumsumPayNotExceedPerNight,
-            rbLumsumPayNotExceedPerYear: masterPlan.rbLumsumPayNotExceedPerYear,
-            rbSchedulePatient: masterPlan.rbSchedulePatient,
-            rbScheduleIntensiveCarePatient: masterPlan.rbScheduleIntensiveCarePatient,
-            rbScheduleDoctor: masterPlan.rbScheduleDoctor,
-            rbScheduleSurgerySchedule: masterPlan.rbScheduleSurgerySchedule,
-            rbScheduleSurgeryNonSchedule: masterPlan.rbScheduleSurgeryNonSchedule,
-            rbScheduleService: masterPlan.rbScheduleService,
-            rbScheduleSmallSurgery: masterPlan.rbScheduleSmallSurgery,
-            rbScheduleAdviser: masterPlan.rbScheduleAdviser,
-            rbScheduleAmbulance: masterPlan.rbScheduleAmbulance,
-            rbScheduleAccident: masterPlan.rbScheduleAccident,
-            rbScheduleTreatment: masterPlan.rbScheduleTreatment,
-            rbScheduleTransplant: masterPlan.rbScheduleTransplant,
-            ipdCoPay: masterPlan.ipdCoPay,
-            ipdCoPayQuota: masterPlan.ipdCoPayQuota,
-            ipdCoPayDeductable: masterPlan.ipdCoPayDeductable,
-            ipdCoPayMixPercentage: masterPlan.ipdCoPayMixPercentage,
-            ipdCoPayMixNotExceed: masterPlan.ipdCoPayMixNotExceed,
-            ipdCoPayMixYear: masterPlan.ipdCoPayMixYear,
-            opdPerYear: masterPlan.opdPerYear,
-            opdPerTime: masterPlan.opdPerTime,
-            opdTimeNotExceedPerYear: masterPlan.opdTimeNotExceedPerYear,
-            opdCoPay: masterPlan.opdCoPay,
-            opdCoPayQuota: masterPlan.opdCoPayQuota,
-            opdCoPayDeductable: masterPlan.opdCoPayDeductable,
-            opdCoPayMixPercentage: masterPlan.opdCoPayMixPercentage,
-            opdCoPayMixNotExceed: masterPlan.opdCoPayMixNotExceed,
-            opdCoPayMixYear: masterPlan.opdCoPayMixYear,
-            lifePerYear: masterPlan.lifePerYear,
-            lifeTimeOfSalary: masterPlan.lifeTimeOfSalary,
-            lifeNotExceed: masterPlan.lifeNotExceed,
-            dentalPerYear: masterPlan.dentalPerYear,
-          }}, () => reply({message: 'create extended plan completed!'}));
+          reply({message: 'create extended plan completed!'});
         });
       });
   },
