f7f72da7235f9747b2a5b9fc8919ccd54233b24f
doschnn <detective-i@hotmail.com>
2017-09-26 00:34:09 +0700

    fixed api summary employee benefit

--- src/api/employee-company.js
+++ src/api/employee-company.js
@@ -547,14 +547,14 @@ const summaryEmployeeBenefit = {
 
   handler: (request, reply) => {
     const { user } = request.auth.credentials;
-    const today = new Date();
-    EmployeeCompany.populate(user, {path: 'company.detail', select: 'effectiveDate expiredDate'}, (err, result) => {
-      const { effectiveDate, expiredDate } = result.company.detail;
+    EmployeeCompany.populate(user, {path: 'company.detail', select: 'startInsurance expiredInsurance'}, (err, result) => {
+      const effectiveDate = result.company.detail.startInsurance;
+      const expiredDate = result.company.detail.expiredInsurance;
       BenefitPlan.find({ company: user.company.detail, effectiveDate, expiredDate})
       .exec((err, benefitPlans) => {
         benefitPlans = benefitPlans.map(benefitPlan => benefitPlan._id);
         const aggregatorOpts = [
-          { $match: { company: user.company.detail, benefitPlan: { $in: benefitPlans }}},
+          { $match: { company: user.company.detail._id, benefitPlan: { $in: benefitPlans }}},
           {
             $group: {
               _id: { benefitPlan: '$benefitPlan', selectGroup: '$selectGroup', confirm: '$confirm'},
@@ -584,81 +584,7 @@ const summaryEmployeeBenefit = {
                 let amountOfPlan = [];
                 let defaultPlan;
                 let type;
-                const inProcess = new Promise((resolve) => {
-                  EmployeeGroup.findOne({ groupName: group._id, company: user.company.detail })
-                  .populate('benefitPlan defaultPlan')
-                  .exec((err, empGroup) => {
-                    type = empGroup.type;
-                    plan = empGroup.benefitPlan.map(element => element.benefitPlanName);
-                    defaultPlan = empGroup.defaultPlan.benefitPlanName;
-                    plan.map(() => {
-                      confirm.push(0);
-                      amountOfPlan.push(0);
-                    });
-                    group.benefitPlan.map((benefitPlan, index) => {
-                      const i = plan.indexOf(benefitPlan.benefitPlanName);
-                      if (group.confirm[index]) {
-                        confirm[i] += group.countPerPlan[index];
-                      }
-                      amountOfPlan[i] += group.countPerPlan[index];
-                    });
-                    resolve();
-                  });
-                });
                 
-                inProcess.then(() => {
-                  resolve(Object.assign({}, {
-                    groupName: group._id,
-                    plan,
-                    confirm,
-                    amountOfPlan,
-                    defaultPlan,
-                    totalOfGroup: group.total,
-                    type,
-                  }));
-                });
-              });
-            });
-            Promise.all(summary).then((groups) => reply(groups));
-          });
-        });
-      });
-    });
-    BenefitPlan.find({ company: user.company.detail, timeout: { $gte: today }})
-    .exec((err, benefitPlans) => {
-      benefitPlans = benefitPlans.map(benefitPlan => benefitPlan._id);
-      const aggregatorOpts = [
-        { $match: { company: user.company.detail, benefitPlan: { $in: benefitPlans }}},
-        {
-          $group: {
-            _id: { benefitPlan: '$benefitPlan', selectGroup: '$selectGroup', confirm: '$confirm'},
-            countAll: { $sum: 1 },
-          },
-        },
-        {
-          $group: {
-            _id: '$_id.selectGroup',
-            benefitPlan: { $push: '$_id.benefitPlan' },
-            confirm: { $push: '$_id.confirm' },
-            countPerPlan: { $push: '$countAll' },
-            total: { $sum: '$countAll' },
-          }, 
-        },
-        {
-          $sort: { _id: 1 }
-        }
-      ];
-      EmployeePlan.aggregate(aggregatorOpts)
-      .exec((err, plans) => {
-        BenefitPlan.populate(plans, {path: 'benefitPlan', select: 'benefitPlanName'}, (err, groups) => {
-          const summary = groups.map((group) => {
-            return new Promise((resolve) => {
-              let plan = [];
-              let confirm = [];
-              let amountOfPlan = [];
-              let defaultPlan;
-              let type;
-              const inProcess = new Promise((resolve) => {
                 EmployeeGroup.findOne({ groupName: group._id, company: user.company.detail })
                 .populate('benefitPlan defaultPlan')
                 .exec((err, empGroup) => {
@@ -676,24 +602,21 @@ const summaryEmployeeBenefit = {
                     }
                     amountOfPlan[i] += group.countPerPlan[index];
                   });
-                  resolve();
+                }).then(() => {
+                  resolve(Object.assign({}, {
+                    groupName: group._id,
+                    plan,
+                    confirm,
+                    amountOfPlan,
+                    defaultPlan,
+                    totalOfGroup: group.total,
+                    type,
+                  }));
                 });
               });
-              
-              inProcess.then(() => {
-                resolve(Object.assign({}, {
-                  groupName: group._id,
-                  plan,
-                  confirm,
-                  amountOfPlan,
-                  defaultPlan,
-                  totalOfGroup: group.total,
-                  type,
-                }));
-              });
             });
+            Promise.all(summary).then((groups) => reply(groups));
           });
-          Promise.all(summary).then((groups) => reply(groups));
         });
       });
     });
