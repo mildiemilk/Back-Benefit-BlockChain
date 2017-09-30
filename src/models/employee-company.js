ae8ef2a49c9a63b7836bb05ed4bf4ea2c980f2a0
doschnn <detective-i@hotmail.com>
2017-09-30 17:08:23 +0700

    the return of enum

--- src/models/employee-company.js
+++ src/models/employee-company.js
@@ -8,7 +8,7 @@ const EmployeeCompanySchema = new mongoose.Schema ({
   companyId: {type: Number, default: 0, unique: true},
   companyName: { type: String, required: true },
   location: { type: String, required: true},
-  typeOfBusiness: { type: String, required: true },
+  typeOfBusiness: { type: String, required: true, enum: ['Type 1', 'Type 2', 'Type 3']},
   hrDetail: {type: String, required: true},
   numberOfEmployees: { type: Number, required: true },
   tel: { type: String, required: true },
