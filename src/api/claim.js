80cd3a35a6febb06ef2bac4986eb5a00e0d65552
chonthicha <punthira.dits@gmail.com>
2017-11-01 14:37:51 +0700

    query all claim

--- src/api/claim.js
+++ src/api/claim.js
@@ -387,18 +387,18 @@ const queryClaim = {
       return fabric_client.getUserContext('user1', true);
     }).then((user_from_store) => {
       if (user_from_store && user_from_store.isEnrolled()) {
-          console.log('Successfully loaded user1 from persistence');
-          member_user = user_from_store;
+        console.log('Successfully loaded user1 from persistence');
+        member_user = user_from_store;
       } else {
-          throw new Error('Failed to get user1.... run registerUser.js');
+        throw new Error('Failed to get user1.... run registerUser.js');
       }
 
       // queryAllTuna - requires no arguments , ex: args: [''],
       const request = {
-          chaincodeId: 'tuna-app',
-          txId: tx_id,
-          fcn: 'queryAllTuna',
-          args: ['']
+        chaincodeId: 'tuna-app',
+        txId: tx_id,
+        fcn: 'queryAllTuna',
+        args: ['']
       };
 
       // send the query proposal to the peer
@@ -407,14 +407,14 @@ const queryClaim = {
       console.log("Query has completed, checking results");
       // query_responses could have more than one  results if there multiple peers were used as targets
       if (query_responses && query_responses.length == 1) {
-          if (query_responses[0] instanceof Error) {
-              console.error("error from query = ", query_responses[0]);
-          } else {
-              console.log("Response is ", query_responses[0].toString());
-              reply(JSON.parse(query_responses[0].toString()));
-          }
+        if (query_responses[0] instanceof Error) {
+          console.error("error from query = ", query_responses[0]);
+        } else {
+          console.log("Response is ", query_responses[0].toString());
+          reply(JSON.parse(query_responses[0].toString()));
+        }
       } else {
-          console.log("No payloads were returned from query");
+        console.log("No payloads were returned from query");
       }
     }).catch((err) => {
       console.error('Failed to query successfully :: ' + err);
