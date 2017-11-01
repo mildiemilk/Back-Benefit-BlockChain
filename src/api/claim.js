c1ab48a40953e5755dc9172f883016daa952afd6
The Do <The Do>
2017-11-01 14:12:53 +0700

    'query'

--- src/api/claim.js
+++ src/api/claim.js
@@ -2,6 +2,9 @@ import Joi from 'joi';
 import Boom from 'boom';
 import mongoose from 'mongoose';
 import moment from 'moment';
+import os  from 'os';
+import Fabric_Client from 'fabric-client';
+import path          from 'path';
 import { LogUserClaim, User, EmployeeCompany, Role, BenefitPlan } from '../models';
 
 const getClaimListCompany = {
@@ -351,6 +354,74 @@ const userClaim = {
   },
 };
 
+const queryClaim = {
+  tags: ['api'],
+  handler: (request, reply) => {
+    console.log("getting all tuna from database: ");
+    var fabric_client = new Fabric_Client();
+
+    // setup the fabric network
+    var channel = fabric_client.newChannel('mychannel');
+    var peer = fabric_client.newPeer('grpc://localhost:7051');
+    channel.addPeer(peer);
+
+    //
+    var member_user = null;
+    var store_path = path.join(os.homedir(), '.hfc-key-store');
+    console.log('Store path:'+store_path);
+    var tx_id = null;
+
+    // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+    Fabric_Client.newDefaultKeyValueStore({ path: store_path
+    }).then((state_store) => {
+      // assign the store to the fabric client
+      fabric_client.setStateStore(state_store);
+      var crypto_suite = Fabric_Client.newCryptoSuite();
+      // use the same location for the state store (where the users' certificate are kept)
+      // and the crypto store (where the users' keys are kept)
+      var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+      crypto_suite.setCryptoKeyStore(crypto_store);
+      fabric_client.setCryptoSuite(crypto_suite);
+
+      // get the enrolled user from persistence, this user will sign all requests
+      return fabric_client.getUserContext('user1', true);
+    }).then((user_from_store) => {
+      if (user_from_store && user_from_store.isEnrolled()) {
+          console.log('Successfully loaded user1 from persistence');
+          member_user = user_from_store;
+      } else {
+          throw new Error('Failed to get user1.... run registerUser.js');
+      }
+
+      // queryAllTuna - requires no arguments , ex: args: [''],
+      const request = {
+          chaincodeId: 'tuna-app',
+          txId: tx_id,
+          fcn: 'queryAllTuna',
+          args: ['']
+      };
+
+      // send the query proposal to the peer
+      return channel.queryByChaincode(request);
+    }).then((query_responses) => {
+      console.log("Query has completed, checking results");
+      // query_responses could have more than one  results if there multiple peers were used as targets
+      if (query_responses && query_responses.length == 1) {
+          if (query_responses[0] instanceof Error) {
+              console.error("error from query = ", query_responses[0]);
+          } else {
+              console.log("Response is ", query_responses[0].toString());
+              reply(JSON.parse(query_responses[0].toString()));
+          }
+      } else {
+          console.log("No payloads were returned from query");
+      }
+    }).catch((err) => {
+      console.error('Failed to query successfully :: ' + err);
+    });
+  },
+};
+
 export default function(app) {
   app.route([
     { method: 'GET', path: '/company/get-claim-list', config: getClaimListCompany },
@@ -359,5 +430,6 @@ export default function(app) {
     { method: 'GET', path: '/insurer/claim-all-company', config: claimAllCompany },
     { method: 'GET', path: '/insurer/get-claim/{companyId}', config: getClaim },
     { method: 'PUT', path: '/insurer/claim/{status}/{claimId}', config: insurerClaim },
+    { method: 'GET', path: '/query-claim', config: queryClaim },
   ]);
 }
